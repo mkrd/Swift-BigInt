dea21eb58b4e888ca9838e22ac7197bc6a7fbd94
yuzushioh <yuzushioh@gmail.com>
2018-02-23 00:45:44 +0900

    add iOS cocoa framework

---
+++ BigNumber-iOSTests/BigNumber_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  BigNumber_iOSTests.swift
+//  BigNumber-iOSTests
+//
+//  Created by yuzushioh on 2018/02/23.
+//  Copyright © 2018 Marcel Kröker. All rights reserved.
+//
+
+import XCTest
+@testable import BigNumber_iOS
+
+class BigNumber_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
