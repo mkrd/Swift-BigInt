f16079158d782b6018b65d6b0d22afa0193c5270
twodayslate <zac@gorak.us>
2018-03-02 00:57:46 -0500

    more test cases

--- Tests/BDoubleTests.swift
+++ Tests/BDoubleTests.swift
@@ -183,6 +183,25 @@ class BDoubleTests : XCTestCase {
 		XCTAssert(BDouble("11", radix: 2) == 3)
 		XCTAssert(BDouble("0b11", radix: 2) == 3)
 		XCTAssert(BDouble("invalid", radix: 2) == nil)
+		
+		XCTAssert(BDouble("ffff",radix:16) == 65535)
+		XCTAssert(BDouble("rfff",radix:16) == nil)
+		XCTAssert(BDouble("ff",radix:10) == nil)
+		XCTAssert(BDouble("255",radix:6) == 107)
+		XCTAssert(BDouble("999",radix:10) == 999)
+		XCTAssert(BDouble("ff",radix:16) == 255.0)
+		XCTAssert(BDouble("ff",radix:16) != 100.0)
+		XCTAssert(BDouble("ffff",radix:16)! > 255.0)
+		XCTAssert(BDouble("f",radix:16)! < 255.0)
+		XCTAssert(BDouble("0",radix:16)! <= 1.0)
+		XCTAssert(BDouble("f",radix:16)! >= 1.0)
+		XCTAssert(BDouble("44",radix:5) == 24)
+		XCTAssert(BDouble("44",radix:5) != 100.0)
+		XCTAssert(BDouble("321",radix:5)! == 86)
+		XCTAssert(BDouble("3",radix:5)! < 255.0)
+		XCTAssert(BDouble("0",radix:5)! <= 1.0)
+		XCTAssert(BDouble("4",radix:5)! >= 1.0)
+		XCTAssert(BDouble("923492349",radix:32)! == 9967689075849)
 	}
 	
 	func testOperations() {
