1ab4d3386363a72f1bba5a39058401ab60bfd26d
twodayslate <zac@gorak.us>
2018-08-06 23:50:31 -0400

    merge with master

--- Sources/Swift-Big-Number-Core.swift
+++ Sources/Swift-Big-Number-Core.swift
@@ -2539,50 +2539,6 @@ public struct BDouble:
 
         return retVal
     }
-	public func decimalExpansion(precisionAfterDecimalPoint precision: Int, rounded : Bool = true) -> String
-	{
-		var currentPrecision = precision
-		
-		if(rounded && precision > 0) {
-			currentPrecision = currentPrecision + 1
-		}
-		
-		let multiplier = [10].exponentiating(currentPrecision)
-		let limbs = self.numerator.multiplyingBy(multiplier).divMod(self.denominator).quotient
-		var res = BInt(limbs: limbs).description
-
-		if currentPrecision <= res.count
-		{
-			res.insert(".", at: String.Index(encodedOffset: res.count - currentPrecision))
-			if res.hasPrefix(".") { res = "0" + res }
-			else if res.hasSuffix(".") { res += "0" }
-		}
-		else
-		{
-			res = "0." + String(repeating: "0", count: currentPrecision - res.count) + res
-		}
-
-		var retVal = self.isNegative() && !limbs.equalTo(0) ? "-" + res : res
-		
-		if(rounded && precision > 0) {
-		
-			let lastDigit = Int(retVal.suffix(1))! // this should always be a number
-			let secondDigit = retVal.suffix(2).prefix(1) // this could be a decimal
-			
-			retVal = String(retVal.prefix(retVal.count-2))
-			if (secondDigit != ".") {
-				if lastDigit >= 5 {
-					retVal = retVal + String(Int(secondDigit)! + 1)
-				} else {
-					retVal = retVal + String(Int(secondDigit)!)
-				}
-			} else {
-				retVal = retVal + "." + String(lastDigit)
-			}
-		}
-		
-		return retVal
-	}
 
 	public var hashValue: Int
 	{
